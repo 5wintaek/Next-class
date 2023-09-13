6d7525670b5c8f600be72e5bb10bfac628a2a402
Seung taek Oh <elqkddl@naver.com>
2023-09-13 17:22:07 +0900

    [Feat] input 창을 이용하여 variables 값 넣기

--- pages/graphql-mutation-args/index.js
+++ pages/graphql-mutation-args/index.js
@@ -1,9 +1,12 @@
 import { gql, useMutation } from '@apollo/client';
+import { useState } from 'react';
 
 //등록
+// 1. 첫번쨰로 타입지정
+// 2. $는 변수타입 지정
 const setGraphql = gql`
-  mutation {
-    createBoard(writer: "철수", title: "제목입니다", contents: "dummy") {
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
       _id
       number
       message
@@ -12,19 +15,45 @@ const setGraphql = gql`
 `;
 
 export default function GraphqlMutationPage() {
-  const [createBoard] = useMutation(setGraphql);
+  const [writer, setWriter] = useState();
+  const [title, setTitle] = useState();
+  const [contents, setContents] = useState();
+  const [createBoardMutation] = useMutation(setGraphql);
 
   const onClickSubmit = async () => {
-    const result = await createBoard({
-      // await 을 빼주면 promise 상태로 변환
-      variables: {
-        writer: '철수',
-        title: '제목입니다',
-        content: 'dummy',
-      },
-    });
-    console.log(result);
+    try {
+      const result = await createBoardMutation({
+        // await 을 빼주면 promise 상태로 변환
+        variables: {
+          // variables => $ 역할을함
+          writer: writer,
+          title: title,
+          contents: contents,
+        },
+      });
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>;
+  // 값을 뽑을려면 e.target.value 필요 (state에 저장)
+  const onChangeWriter = (e) => {
+    setWriter(e.target.value);
+  };
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeContents = (e) => {
+    setContents(e.target.value);
+  };
+
+  return (
+    <div>
+      작성자 : <input type="text" onChange={onChangeWriter} />
+      제목 : <input type="text" onChange={onChangeTitle} />
+      내용 : <input type="text" onChange={onChangeContents} />
+      <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+    </div>
+  );
 }
